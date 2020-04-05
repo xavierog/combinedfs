ebd1f67c55a856deb962646f98c6730a128a7aac
Xavier G <xavier@kindwolf.org>
2020-04-05 15:07:57 +0000

    Add copyright and license.

--- combinedfs.py
+++ combinedfs.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# Copyright © 2020 Xavier G. <xavier.combinedfs@kindwolf.org>
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the COPYING file for more details.
+
 import os
 import re
 import sys
